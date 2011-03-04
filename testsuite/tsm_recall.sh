db413921054c31764ba42d1d3e899c098ce07f62
Daniele Gregori <daniele.gregori@cnaf.infn.it>
2011-03-04 12:18:10 +0000

    modifiche al codice per avere file con dimensione variabile

--- testsuite/tsm_recall.sh
+++ testsuite/tsm_recall.sh
@@ -3,6 +3,10 @@
 echo CREA LISTE DEI DEI FILE MIGRATI IN PRECEDENZA PER FARE RECALL ORDINATE
 echo OCCORRE ESSERE SU TSM-HSM-13 (owner node)
 
+NFILE=$(cat testsuite.conf|egrep -v '^#'|egrep '^' NFILE|sed -e 's/^.*=//')
+
+NMEZZI= $(($NFILE/2));
+
 mkdir /tmp/greg
 rm -f /tmp/greg/*
 touch /tmp/greg/lista.tmp
@@ -11,19 +15,23 @@ mkdir /tmp/greg2
 rm -f /tmp/greg2/*
 touch /tmp/greg2/lista.tmp
 
-for i in `seq 4`; do echo /gpfs/gemss_test/test/data1/greg$i>> /tmp/greg/lista.tmp; 
-echo /gpfs/gemss_test/test/data2/greg$i>> /tmp/greg/lista.tmp;
-echo /gpfs/gemss_test2/test/data3/greg$i>> /tmp/greg/lista.tmp; 
-echo /gpfs/gemss_test2/test/data4/greg$i>> /tmp/greg/lista.tmp;
+for i in `seq $NMEZZI` 
+do 
+    echo /gpfs/gemss_test/test/data1/greg$i>> /tmp/greg/lista.tmp; 
+    echo /gpfs/gemss_test/test/data2/greg$i>> /tmp/greg/lista.tmp;
+    echo /gpfs/gemss_test2/test/data3/greg$i>> /tmp/greg/lista.tmp; 
+    echo /gpfs/gemss_test2/test/data4/greg$i>> /tmp/greg/lista.tmp;
 done
 
 cd /tmp/greg/ 
 dsmrecall -t -p -fi=/tmp/greg/lista.tmp
 
-for i in `seq 5 9`; do echo /gpfs/gemss_test/test/data1/greg$i>> /tmp/greg2/lista.tmp; 
-echo /gpfs/gemss_test/test/data2/greg$i>> /tmp/greg2/lista.tmp;
-echo /gpfs/gemss_test2/test/data3/greg$i>> /tmp/greg2/lista.tmp; 
-echo /gpfs/gemss_test2/test/data4/greg$i>> /tmp/greg2/lista.tmp;
+for i in `seq $(($NMEZZI+1)) NFILE` 
+do 
+    echo /gpfs/gemss_test/test/data1/greg$i>> /tmp/greg2/lista.tmp; 
+    echo /gpfs/gemss_test/test/data2/greg$i>> /tmp/greg2/lista.tmp;
+    echo /gpfs/gemss_test2/test/data3/greg$i>> /tmp/greg2/lista.tmp;  
+    echo /gpfs/gemss_test2/test/data4/greg$i>> /tmp/greg2/lista.tmp;
 done
 
 cd /tmp/greg2/ 
