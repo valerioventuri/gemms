7e94483b2fb730110fdf48856e06ecd277d086dc
Daniele Gregori <daniele.gregori@cnaf.infn.it>
2011-03-04 12:05:28 +0000

    modifiche al codice per avere file con dimensione variabile

--- testsuite/tsm_premigrate.sh
+++ testsuite/tsm_premigrate.sh
@@ -2,17 +2,29 @@
 
 echo CREAZIONE DEI FILE DA preMIGRARE
 
-for i in `seq 9`; do dd if=/dev/urandom of=/gpfs/gemss_test/test/data1/greg$i bs=2048 count=1000; done
+BASEPATH1="/gpfs/gemss_test/test/"
+BASEPATH2="/gpfs/gemss_test2/test/"
 
-for i in `seq 9`; do dd if=/dev/urandom of=/gpfs/gemss_test/test/data2/greg$i bs=2048 count=1000; done
+NFILE=10;
+OFFSET=100;
 
-for i in `seq 9`; do dd if=/dev/urandom of=/gpfs/gemss_test2/test/data3/greg$i bs=2048 count=1000; done
+for i in `seq $NFILE` 
+do 
 
-for i in `seq 9`; do dd if=/dev/urandom of=/gpfs/gemss_test2/test/data4/greg$i bs=2048 count=1000; done
+    cnt=`echo $(($RANDOM %1000 + $OFFSET))` 
+    dd if=/dev/urandom of=${BASEPATH1}/data1/greg$i bs=2048 count=$cnt 
+    cnt=`echo $(($RANDOM %1000 + $OFFSET))`
+    dd if=/dev/urandom of=${BASEPATH1}/data2/greg$i bs=2048 count=$cnt 
+    cnt=`echo $(($RANDOM %1000 + $OFFSET))`
+    dd if=/dev/urandom of=${BASEPATH2}/data3/greg$i bs=2048 count=$cnt 
+    cnt=`echo $(($RANDOM %1000 + $OFFSET))`
+    dd if=/dev/urandom of=${BASEPATH2}/data4/greg$i bs=2048 count=$cnt 
+done
 
 echo ATTESA DI 5 minuti CHE IL FILE INVECCHI poi si migrano;
 sleep 300
-
+date
+echo "LANCIO dsmmigrate"
 ssh tsm-hsm-13 "dsmmigrate -p /gpfs/gemss_test/test/data1/greg*"
 
 ssh tsm-hsm-12 "dsmmigrate -p /gpfs/gemss_test/test/data2/greg*"
@@ -21,5 +33,6 @@ ssh tsm-hsm-13 "dsmmigrate -p /gpfs/gemss_test2/test/data3/greg*"
 
 ssh tsm-hsm-12 "dsmmigrate -p /gpfs/gemss_test2/test/data4/greg*"
 
+date
 echo CONTROLLARE I LOG IN /gpfs/gemss_test/system/YAMSS_LOG/startpolicy.log
 echo E IN /gpfs/gemss_test2/system/YAMSS_LOG/startpolicy.log
